095046993ab49cbaed2f6c1909293a0623938b8e
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-08-25 13:53:37 -0700

    partial implementation of compute_slice_statistic(); currently just pulls ROC for each subgroup

--- slice/slice_utils.R
+++ slice/slice_utils.R
@@ -38,6 +38,32 @@ compute_roc <- function(preds, labs){
     return(perf)
 }
 
+## df: dataframe containing colnames matching pred_col, label_col, and protected_attr_col
+## pred_col: name of column containing predicted probabilities
+## label_col: name of column containing true labels (should be 0,1 only)
+## protected_attr_col: name of column containing protected attr
+## majority_protected_attr_val: name of "majority" group wrt protected attribute
+## returns: value of slice statistic, absolute value of area between ROC curves for protected_attr_col
+compute_slice_statistic <- function(df, pred_col, label_col, protected_attr_col, majority_protected_attr_val){
+    # todo: input checking
+        # pred_col should be in interval [0,1]
+        # label_col should be strictly 0 or 1
+        # majority_protected_attr_col should be in protected_attr_col values
+        # protected_attr_col must be factor, otherwise convert and warn
+    # initialize data structures
+    ss = 0
+    protected_attr_vals = unique(df[,protected_attr_col])
+    roc_list = list()
+    # compute roc within each group of protected_attr_vals
+    for (protected_attr_val in protected_attr_vals){
+        protected_attr_df = df[df[,protected_attr_col] == protected_attr_val,]
+        roc_list[[protected_attr_val]] = compute_roc(protected_attr_df[,pred_col], protected_attr_df[,label_col])
+    }
+    # compare each non-majority class to majority class; accumulate absolute difference between ROC curves to slicing statistic
+    for (protected_attr_val in protected_attr_vals[protected_attr_vals != majority_protected_attr_val]){
+        
+    }
+}
 
 
 
