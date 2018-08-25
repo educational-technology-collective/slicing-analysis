96482d45699e589bc13456cdef45196597fff515
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-08-25 11:25:25 -0700

    completed version

--- create_master_hash_mapping.py
+++ create_master_hash_mapping.py
@@ -10,19 +10,18 @@ import os
 import tempfile
 import re
 
-
-GENDER_CSV_FP = os.path.join(os.getcwd(), "data/names_for_josh.csv") # docker doesn't like relative file paths
+GENDER_CSV_FP = os.path.join(os.getcwd(), "data/names_for_josh.csv")  # docker doesn't like relative file paths
 GENDER_VALUES_TO_KEEP = ("male", "female")
 MORF_DATA_DIR = "morf-data/"
 MYSQL_DOCKER_DIR = os.path.join(os.getcwd(), "docker")
 MYSQL_DOCKER_IMG_NAME = "mysql-docker.tar"
 OUTPUT_DIR = os.path.join(os.getcwd(), "data/hash-mapping-exports")
-
+OUTPUT_FILENAME = "coursera_user_hash_gender_lookup.csv"
+GENDER_COL_NAME = "gender"
 
 module_logger = logging.getLogger(__name__)
 job_config = MorfJobConfig("config.properties")
 logger = set_logger_handlers(module_logger, job_config)
-
 
 for raw_data_bucket in job_config.raw_data_buckets:
     for course in fetch_complete_courses(job_config, raw_data_bucket):
@@ -31,19 +30,21 @@ for raw_data_bucket in job_config.raw_data_buckets:
             with tempfile.TemporaryDirectory(dir=os.getcwd()) as working_dir:
                 print("[INFO] processing course {} session {}".format(course, session))
                 # download the data exports
-                download_raw_course_data(job_config, raw_data_bucket, course=course, session=session, input_dir=working_dir,
-                                         data_dir=MORF_DATA_DIR[:-1]) # drop trailing slash on data dir
+                download_raw_course_data(job_config, raw_data_bucket, course=course, session=session,
+                                         input_dir=working_dir,
+                                         data_dir=MORF_DATA_DIR[:-1])  # drop trailing slash on data dir
                 # create docker run command and load image
                 image_uuid = load_docker_image(MYSQL_DOCKER_DIR, job_config, logger, image_name=MYSQL_DOCKER_IMG_NAME)
-                cmd = make_docker_run_command(job_config.docker_exec, working_dir, OUTPUT_DIR, image_uuid, course=course, session=session, mode=None,
-                                        client_args=None)
+                cmd = make_docker_run_command(job_config.docker_exec, working_dir, OUTPUT_DIR, image_uuid,
+                                              course=course, session=session, mode=None,
+                                              client_args=None)
                 # run the docker image, make sure to pass params for course and session
                 execute_and_log_output(cmd, logger)
 
 # concatenate into single file in OUTPUT_DIR
 df_list = []
 for f in os.listdir(OUTPUT_DIR):
-    if f.endswith(".csv") and not f.startswith("."): # only use csv files, ignore system files
+    if f.endswith(".csv") and not f.startswith("."):  # only use csv files, ignore system files
         print("[INFO] reading file {}".format(f))
         # pull course name and session from filename
         regex = re.compile("^hash_mapping_(\S+)_([0-9\-]+).*")
@@ -56,12 +57,16 @@ for f in os.listdir(OUTPUT_DIR):
         df["session"] = session
         df_list.append(df)
 hash_mapping_df = pd.concat(df_list)
-print("[INFO] hash mapping table contains {} rows".format(hash_mapping_df.shape[0]))
+print("[INFO] concatenated hash mapping table contains {} rows".format(hash_mapping_df.shape[0]))
 
 # read in gender
 gender_df = pd.read_csv(GENDER_CSV_FP).drop_duplicates()
 print("[INFO] original gender_df file contains {} rows".format(gender_df.shape[0]))
-gender_df = gender_df.loc[gender_df['gender'].isin(GENDER_VALUES_TO_KEEP)]
+gender_df = gender_df.loc[gender_df[GENDER_COL_NAME].isin(GENDER_VALUES_TO_KEEP)]
 print("[INFO] after dropping low-confidence gender guesses gender_df contains {} rows".format(gender_df.shape[0]))
 
-
+# merge results and write to OUTPUT_DIR
+df_out = pd.merge(hash_mapping_df, gender_df, left_on="user_id", right_on="coursera_user_id", how="inner")
+print("[INFO] gender balance in final output dataset:")
+print(df_out[GENDER_COL_NAME].value_counts())
+df_out.to_csv(os.path.join(OUTPUT_DIR, OUTPUT_FILENAME))
