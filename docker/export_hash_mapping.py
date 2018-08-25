d1f913d216ee0c6ed00757af7d7457247fb3b256
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-08-24 17:00:08 -0700

    added some more informative printing

--- docker/export_hash_mapping.py
+++ docker/export_hash_mapping.py
@@ -4,22 +4,23 @@ A script to load the hash happing tables from coursera spark data exports and ex
 
 from morf_slice_utils import *
 import argparse
-import pandas as pd
 import os
 
 
 def main(course, session, output_dir="/output"):
-    outfilename = "/hash_mapping_{}_{}.csv".format(course, session)
+    outfilename = "hash_mapping_{}_{}.csv".format(course, session)
     unzip_sql_dumps(course, session)
     load_data(course, session)
     execute_mysql_query_into_csv("SELECT * FROM hash_mapping", os.path.join(output_dir, outfilename))
+    print("[INFO] files in output_dir: ".format(os.listdir(output_dir)))
     return
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="execute feature extraction, training, or testing.")
     parser.add_argument("-c", "--course", required=True, help="an s3 pointer to a course")
-    parser.add_argument("-r", "--session", required=False, help="3-digit course run number")
+    parser.add_argument("-r", "--session", required=True, help="3-digit course run number")
+    parser.add_argument("--mode", required=False, help="mode; not used but automatically passed to docker by most MORF API functions")
     args = parser.parse_args()
     main(args.course, args.session)
 
