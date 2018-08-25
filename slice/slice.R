2577680fc2424464892528b9f95751d66830ab27
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-08-25 13:20:43 -0700

    data wranging section complete

--- slice/slice.R
+++ slice/slice.R
@@ -1,13 +1,26 @@
 # script to conduct sample slicing analysis
+source("slice_utils.R")
 
 # module-level variables
 data_dir = "../data"
 pred_csv = "josh_gardner-dl-replication-week3-lstm-test.csv"
 label_csv = "labels-test-michigan.csv"
-protected_attr_csv = "names_for_josh.csv"
-course = "sna" # eventually, script should iterate over courses in "course" column of pred_csv
+protected_attr_csv = "hash-mapping-exports/coursera_user_hash_gender_lookup.csv"
 
-pred_df = read.csv(file.path(data_dir, pred_csv))
-label_df = read.csv(file.path(data_dir, label_csv))
-protected_attr_df = read.csv(file.path(data_dir, label_csv))
+#temporary; remove this later as script iterates over courses
+course = "pythonlearn"
 
+#setwd(".")
+pred_df = read.csv(file.path(data_dir, pred_csv), stringsAsFactors = FALSE)
+label_df = read.csv(file.path(data_dir, label_csv), stringsAsFactors = FALSE)
+protected_attr_df = read.csv(file.path(data_dir, protected_attr_csv), stringsAsFactors = FALSE)
+
+
+# create single dataframe with user-course level prediction, label, and protected attributes
+user_course_df <- label_df %>%
+    dplyr::filter(label_type == "dropout") %>%
+    dplyr::inner_join(pred_df) %>%
+    dplyr::inner_join(protected_attr_df, by = c("userID" = "session_user_id", "course" = "course")) %>%
+    dplyr::select(c("userID", "course", "prob", "label_value", "gender"))
+
+# for (course in unique(user_course_df))    
