5f55213852a0e5a6300ad878a8609092025e90da
Joshua Gardner <jpgard@nibbler.si.umich.edu>
2018-09-06 15:31:11 -0400

    working version frmo nibbler; feature extraction in multithread

--- gardner/controller.py
+++ gardner/controller.py
@@ -6,8 +6,8 @@ from morf.workflow.extract import extract_session, extract_holdout_session
 from morf.workflow.train import train_course
 from morf.workflow.test import test_course
 
-extract_session(multithread=False)
-extract_holdout_session(multithread=False)
-train_course(label_type='dropout', multithread=False)
-test_course(label_type='dropout')
-evaluate_course(label_type='dropout')
+extract_session()
+extract_holdout_session()
+# train_course(label_type='dropout', multithread=False)
+# test_course(label_type='dropout')
+# evaluate_course(label_type='dropout')
