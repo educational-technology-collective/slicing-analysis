1130b68986c6a5db1bb494ffa285e7df48d96319
Joshua Gardner <jpgard@nibbler.si.umich.edu>
2018-09-07 15:04:56 -0400

    now actually calls aggregate_and_remove_feature_files; before this call was missing which led to many output CSVs which were never removed

--- gardner/gardner.py
+++ gardner/gardner.py
@@ -5,6 +5,9 @@ Main script to replicate "best" model from Gardner and Brooks (2018)
 import argparse
 import subprocess
 
+INPUT_DIR = "/input"
+OUTPUT_DIR = "/output"
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="execute feature extraction, training, or testing.")
     parser.add_argument("-c", "--course", required=True, help="an s3 pointer to a course")
@@ -15,7 +18,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.mode == "extract":
         from extraction.extract_features import main as extract_features
+        from extraction.extraction_utils import aggregate_and_remove_feature_files
         extract_features(args.course, args.session)
+        aggregate_and_remove_feature_files()
     elif args.mode == "train":
         cmd = "Rscript modeling/build_models.R --course {} --session {} --working_dir /input --output_dir /output --model_type {}".format(args.course, args.session, args.model_type)
         subprocess.call(cmd, shell=True)
