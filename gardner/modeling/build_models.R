01794eda6ee4dfe1d7c0d48e311d125a2a028aed
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-09-10 17:20:06 -0700

    renamed working_dir to input_dir flag

--- gardner/modeling/build_models.R
+++ gardner/modeling/build_models.R
@@ -3,7 +3,7 @@ library(optparse, quietly = TRUE, warn.conflicts = FALSE)
 option_list = list(
     make_option(c("-c", "--course"), type="character", default=NULL, 
                 help="course", metavar="character"),
-    make_option(c("-w", "--working_dir"), type="character", default=NULL,
+    make_option(c("-i", "--input_dir"), type="character", default=NULL,
                 help="current working dir", metavar="character"),
     make_option(c("-o", "--output_dir"), type="character", default=NULL,
                 help="output dir", metavar="character"),
@@ -15,12 +15,12 @@ opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
 
 course = opt$course
-working_dir = opt$working_dir
+input_dir = opt$input_dir
 output_dir = opt$output_dir
 mt = opt$model_type
 
 setwd(working_dir)
 source("modeling/modeling_utils.R")
-mod = build_models(course, working_dir, model_type = mt)
+mod = build_model(course, input_dir, model_type = mt)
 # save mod to file
 save(mod, file = file.path(output_dir, "model.Rdata"))
