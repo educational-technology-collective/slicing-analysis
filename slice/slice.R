e3a378f37ec614414d1bdcba7dda123cea0180ab
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-09-11 18:03:16 -0700

    added command-line options instead of hard-coded params

--- slice/slice.R
+++ slice/slice.R
@@ -1,17 +1,26 @@
 # script to conduct sample slicing analysis
-source("slice_utils.R")
+library(optparse) #, quietly = TRUE, warn.conflicts = FALSE
+option_list = list(
+    make_option("--pred_csv", type="character", default=NULL,
+                help="csv file of predictions", metavar="character"),
+    make_option("--label_csv", type="character", default=NULL,
+                help="csv file of labels", metavar="character"),
+    make_option("--protected_attr_csv", type="character", default=NULL,
+                help="csv file of protected attribute memberships", metavar="character"),
+    make_option("--img_dir", type="character", default=NULL,
+                help="directory to write images to", metavar="character")
+)
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+pred_csv = opt$pred_csv
+label_csv = opt$label_csv
+protected_attr_csv = opt$protected_attr_csv
+img_dir = opt$img_dir
+pred_df = read.csv(pred_csv, stringsAsFactors = FALSE)
+label_df = read.csv(label_csv, stringsAsFactors = FALSE)
+protected_attr_df = read.csv(protected_attr_csv, stringsAsFactors = FALSE)
 
-# module-level variables
-data_dir = "../data"
-pred_csv = "josh_gardner-dl-replication-week3-lstm-test.csv"
-label_csv = "labels-test-michigan.csv"
-protected_attr_csv = "hash-mapping-exports/coursera_user_hash_gender_lookup.csv"
-img_dir = "../img/fei-lstm/michigan"
-
-pred_df = read.csv(file.path(data_dir, pred_csv), stringsAsFactors = FALSE)
-label_df = read.csv(file.path(data_dir, label_csv), stringsAsFactors = FALSE)
-protected_attr_df = read.csv(file.path(data_dir, protected_attr_csv), stringsAsFactors = FALSE)
-
+source("slice/slice_utils.R")
 
 # create single dataframe with user-course level prediction, label, and protected attributes
 user_course_df <- label_df %>%
@@ -20,8 +29,7 @@ user_course_df <- label_df %>%
     dplyr::inner_join(protected_attr_df, by = c("userID" = "session_user_id", "course" = "course")) %>%
     dplyr::select(c("userID", "course", "prob", "label_value", "gender")) %>%
     dplyr::mutate(gender = forcats::as_factor(gender))
-
-
+# compute slice statistic for each course and write slice plots to img_dir
 ss_list = list()
 for (course_name in unique(user_course_df$course)){ 
     message(glue('processing course {course_name}'))
@@ -29,14 +37,18 @@ for (course_name in unique(user_course_df$course)){
         dplyr::filter(course == course_name) %>%
         tibble::column_to_rownames("userID") %>%
         select(c("prob", "label_value", "gender"))
-    ss = compute_slice_statistic(course_df, 
-                                 pred_col = "prob", 
-                                 label_col = "label_value", 
-                                 protected_attr_col = "gender",  
-                                 majority_protected_attr_val = "male",
-                                 image_dir = img_dir,
-                                 course = course_name)
-    ss_list[[course_name]] <- ss
+    if (length(unique(course_df$label_value)) <= 1){
+        message(glue::glue("warning: skipping course {course_name}; must be at least 2 unique label values"))
+    } else{
+        ss = compute_slice_statistic(course_df, 
+                                     pred_col = "prob", 
+                                     label_col = "label_value", 
+                                     protected_attr_col = "gender",  
+                                     majority_protected_attr_val = "male",
+                                     image_dir = img_dir,
+                                     course = course_name)
+        ss_list[[course_name]] <- ss
+    }
 } # end iteration over courses
 
 ## exploratory visualization of results
